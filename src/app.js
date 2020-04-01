0d98241c7d0f2f7a3551d649c46a8d48f083867b
Tom Hinton <tomjhinton@gmail.com>
2020-04-01 14:01:49 +0100

    sidebar

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 
 
 import Main from './components/Main'
+import 'bulma'
 import './style.scss'
 
 class App extends React.Component {
--- src/components/Main.js
+++ src/components/Main.js
@@ -9,10 +9,15 @@ class Main extends React.Component{
     this.state = {
       data: {},
       error: '',
-      size: 10
+      font: 'Passio',
+      fontList: ['Passio', 'Ferrite', '"Art-Dystopia II', 'ChunkFive', 'Orbitron Black', 'Tapeworm'],
+      fontSize: 2,
+      fontColor: 'black',
+      bgColor: 'white'
 
     }
     this.componentDidMount = this.componentDidMount.bind(this)
+    this.fontSelect = this.fontSelect.bind(this)
 
 
 
@@ -42,6 +47,10 @@ class Main extends React.Component{
     // this.setState({bass: `${e.screenX /100000} ${e.screenY /100000} `, scale: `${e.screenY /2}` })
   }
 
+  fontSelect(e){
+    this.setState({font: e.target.innerText})
+  }
+
 
 
   render() {
@@ -50,9 +59,44 @@ class Main extends React.Component{
     console.log(this.state)
     return (
 
-      <div onMouseMove={this.mouseMove} className="body">
+      <div onMouseMove={this.mouseMove} className="container">
 
-        <div className='board'>
+        <div className='columns'>
+          <div className='column is-2'>
+
+            {this.state.fontList.map((x,index)=>{
+              return(
+                <div key={index} onClick={this.fontSelect} style={{fontFamily: x, cursor: 'pointer'}}>
+                  {x}
+                </div>
+
+              )
+
+            })}
+
+          </div>
+          <div className='column'  style={{fontFamily: this.state.font}}>
+            <p>
+            The sky above the port was the color of television, tuned to a dead channel.
+            </p>
+
+            <p>
+            The sky above the port was the color of television, tuned to a dead channel.
+            </p>
+
+            <p>
+            The sky above the port was the color of television, tuned to a dead channel.
+            </p>
+
+            <p>
+            The sky above the port was the color of television, tuned to a dead channel.
+            </p>
+
+            <p>
+            The sky above the port was the color of television, tuned to a dead channel.
+            </p>
+
+          </div>
 
         </div>
 
